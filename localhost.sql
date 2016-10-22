0d45124340a2c428ace2de2f0864f02fc0a9d867
Lance <lance@compulsivetech.biz>
2016-10-21 22:14:53 -0700

    Add files via upload

---
+++ localhost.sql
@@ -0,0 +1,85 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2+deb8u2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 21, 2016 at 10:14 PM
+-- Server version: 5.5.52-0+deb8u1
+-- PHP Version: 5.6.24-0+deb8u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `thot_patrol`
+--
+CREATE DATABASE IF NOT EXISTS `thot_patrol` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `thot_patrol`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE IF NOT EXISTS `report` (
+`uid` int(35) NOT NULL,
+  `thot_name` varchar(75) NOT NULL,
+  `thot_photo` varchar(50) NOT NULL,
+  `thot_by` int(35) NOT NULL,
+  `submitted_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`uid` int(35) NOT NULL,
+  `full_name` varchar(55) NOT NULL,
+  `email` varchar(65) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+ ADD PRIMARY KEY (`uid`), ADD UNIQUE KEY `uid` (`uid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`uid`), ADD UNIQUE KEY `uid` (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+MODIFY `uid` int(35) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `uid` int(35) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
